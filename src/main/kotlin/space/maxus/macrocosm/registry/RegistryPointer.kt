87083a62a60e6df0a47c8d38785f8eade3a1ebf1
maxus <maxus.info.mail@gmail.com>
2022-10-23 13:32:37 +0300

    Added some docs

--- src/main/kotlin/space/maxus/macrocosm/registry/RegistryPointer.kt
+++ src/main/kotlin/space/maxus/macrocosm/registry/RegistryPointer.kt
@@ -5,6 +5,15 @@ import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonWriter
 import space.maxus.macrocosm.util.general.ConditionalCallback
 
+/**
+ * A pointer to a value in certain registry
+ *
+ * @param registry registry this pointer's element belongs to
+ * @param pointer ID of the element
+ *
+ * @property registry registry this pointer's element belongs to
+ * @property pointer ID of the element
+ */
 class RegistryPointer(val registry: Identifier, pointer: Identifier) {
     var pointer: Identifier = pointer; private set
 
@@ -16,6 +25,11 @@ class RegistryPointer(val registry: Identifier, pointer: Identifier) {
         return ConditionalCallback { success }
     }
 
+    /**
+     * Writes a value to this pointer.
+     *
+     * @return false if failed to write value, true if succeeded
+     */
     @Suppress("UNCHECKED_CAST")
     fun <T> set(v: T): Boolean {
         val actualRegistry: Registry<T> = (Registry.find(registry) as? Registry<T>) ?: return false
@@ -23,12 +37,23 @@ class RegistryPointer(val registry: Identifier, pointer: Identifier) {
         return true
     }
 
+    /**
+     * Reads a value from this pointer
+     *
+     * @throws ClassCastException if [T] is not the type of value in this pointer
+     * @throws NullPointerException if it could not find element of ID [pointer] inside the [registry] registry
+     */
     @Suppress("UNCHECKED_CAST")
     fun <T> get(): T {
         val actualRegistry: Registry<T> = (Registry.find(registry) as? Registry<T>)!!
         return actualRegistry.find(pointer)
     }
 
+    /**
+     * Attempts to read value from this pointer
+     *
+     * @return null if failed to read the value, value of the pointer otherwise
+     */
     @Suppress("UNCHECKED_CAST")
     fun <T> tryGet(): T? {
         return try {
@@ -39,10 +64,16 @@ class RegistryPointer(val registry: Identifier, pointer: Identifier) {
         }
     }
 
+    /**
+     * Gets the ID of the registry
+     */
     operator fun component1(): Identifier {
         return this.registry
     }
 
+    /**
+     * Gets the ID of the pointer
+     */
     operator fun component2(): Identifier {
         return this.pointer
     }
@@ -57,6 +88,9 @@ class RegistryPointer(val registry: Identifier, pointer: Identifier) {
         return result
     }
 
+    /**
+     * Converts this pointer to the string representation in format of `registry@pointer`
+     */
     override fun toString(): String {
         return "$registry@$pointer"
     }
