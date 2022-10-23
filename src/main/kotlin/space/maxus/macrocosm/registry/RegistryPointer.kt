450c9ac50460822cc792bed6fe413b15eb76b328
maxus <maxus.info.mail@gmail.com>
2022-10-23 13:13:13 +0300

    Created the RegistryPointer class

--- src/main/kotlin/space/maxus/macrocosm/registry/RegistryPointer.kt
+++ src/main/kotlin/space/maxus/macrocosm/registry/RegistryPointer.kt
@@ -1,14 +1,23 @@
 package space.maxus.macrocosm.registry
 
-data class RegistryPointer(val registry: Identifier, val pointer: Identifier) {
+class RegistryPointer(val registry: Identifier, pointer: Identifier) {
+    var pointer: Identifier = pointer; private set
+
     @Suppress("UNCHECKED_CAST")
-    inline fun <reified T> get(): T {
+    fun <T> trySet(v: T): Boolean {
+        val actualRegistry: Registry<T> = (Registry.find(registry) as? Registry<T>) ?: return false
+        this.pointer = actualRegistry.byValue(v) ?: return false
+        return true
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T> get(): T {
         val actualRegistry: Registry<T> = (Registry.find(registry) as? Registry<T>)!!
         return actualRegistry.find(pointer)
     }
 
     @Suppress("UNCHECKED_CAST")
-    inline fun <reified T> tryGet(): T? {
+    fun <T> tryGet(): T? {
         return try {
             val actualRegistry = Registry.find(registry) as Registry<T>
             actualRegistry.findOrNull(pointer)
